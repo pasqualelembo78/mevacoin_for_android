12f652089fb6265f76ca157257a467c7fa6d0305
Your Name <you@example.com>
2025-09-15 20:27:32 +0200

    Rimuovo file binari grandi

--- build-android-so/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mevacoin/external/cryptopp
-# Build directory: /mevacoin/build-android-so/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
