1fa49f224301dad2101716b39727896a80622f2f
Your Name <you@example.com>
2025-09-16 20:49:43 +0200

    Aggiornamento build Android per generare sia .so che .a

--- external/rocksdb/build-android/build_version.cc
+++ external/rocksdb/build-android/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
 const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2025/09/15 15:55:15@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2025/09/16 17:31:08@";
 const char* rocksdb_build_compile_date = __DATE__;
